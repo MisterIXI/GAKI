ac969ef694d59d5799dc71a14a0da797cf195eb9
Yannik Braendle <yannik.braendle@t-online.de>
2025-07-24 17:42:11 +0200

    functional pong

--- pong/pong_ball.gd
+++ pong/pong_ball.gd
@@ -1,11 +1,9 @@
 class_name PongBall
 extends CharacterBody2D
-
+const BOUNCE_ANGLE: float = 45
 
 func _ready():
 	velocity = Vector2(200, 0)
-	# velocity = velocity.rotated(randf() * PI * 2) # Randomize initial direction
-
 
 func _physics_process(delta):
 	var collision: KinematicCollision2D = move_and_collide(velocity * delta)
@@ -13,28 +11,33 @@ func _physics_process(delta):
 		# check if collided with a paddle
 		var collider: Node = collision.get_collider()
 		if collider.is_in_group("paddles"):
-			handle_paddle_collision(collision)
+			_handle_paddle_collision(collision)
 		else:
 			velocity = velocity.bounce(collision.get_normal())
 	pass
 
-
 func _handle_paddle_collision(collision: KinematicCollision2D) -> void:
+	if collision.get_collider() is not Paddle:
+		push_warning("Collision with non-Paddle object: %s" % collision.get_collider().name)
+		return
 	# if the ball hits a paddle, adjust the angle based on where it hit
-	var paddle: Node2D = collider
+	var paddle: Paddle = collision.get_collider()
 	var paddle_center: Vector2 = paddle.global_position
 	var hit_position: Vector2 = collision.get_position()
 	var diff = hit_position.y - paddle_center.y
-	print("Hit paddle with diff: ", diff)
+	diff = diff / (paddle.size / 2.0)
 	velocity = velocity.bounce(collision.get_normal())
 	var magnitude: float = velocity.length()
-	if diff > 0 and velocity.y < 0:
-		velocity.y = diff
-	elif diff < 0 and velocity.y > 0:
-		velocity.y = -diff
-	else:
-		velocity.y += diff
 	velocity = velocity.normalized() * (magnitude + abs(diff))
-
-func _handle_goal_collision(collision: KinematicCollision2D) -> void:
-	pass
+	# only rotate if angle not already too extreme
+	var vel_angle_deg: float = rad_to_deg(velocity.angle())
+	print("Ball velocity angle: %.2f degrees" % vel_angle_deg)
+	if (
+		# between -40 and 40 (towards right)
+		(vel_angle_deg > -40.0 and vel_angle_deg < 40.0) or
+		# between 140 and -140 (towards left)
+		(vel_angle_deg > 140.0 or vel_angle_deg < -140.0)
+	):
+		# rotate by diff
+		velocity = velocity.rotated(deg_to_rad(diff * BOUNCE_ANGLE))
+		print("Rotated ball by %.2f degrees" % (diff * BOUNCE_ANGLE))
--- pong/pong_match.gd
+++ pong/pong_match.gd
@@ -1,4 +1,38 @@
 extends Node2D
 
+@export var ball: CharacterBody2D
+
 @export var label_a: Label
 @export var label_b: Label
+var score_a: int = 0
+var score_b: int = 0
+
+func _ready():
+	reset_ball(true)
+
+func _on_goal_left_body_entered(body: Node2D) -> void:
+	if body.is_in_group("ball"):
+		score_b += 1
+		label_b.text = str("%02d" % score_b)
+		reset_ball(true)
+		print("Goal for Player B! Score: ", score_b)
+
+func _on_goal_right_body_entered(body: Node2D) -> void:
+	if body.is_in_group("ball"):
+		score_a += 1
+		label_a.text = str("%02d" % score_a)
+		reset_ball(false)
+		print("Goal for Player A! Score: ", score_a)
+
+
+func reset_ball(towards_left: bool = true) -> void:
+	ball.global_position = Vector2(1280.0/2.0, 720.0/2.0) + Vector2.UP * (randf() - 0.5) * 100
+	ball.velocity = Vector2.ZERO
+	await get_tree().create_timer(1.0).timeout
+	if towards_left:
+		ball.velocity = Vector2(-300, 0)
+	else:
+		ball.velocity = Vector2(300, 0)
+	# rotate by 10% of 180 degrees
+	ball.velocity = ball.velocity.rotated(randf() * PI * 0.1)
+	
