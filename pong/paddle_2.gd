3f9c776721eb1f21ba0939d5664362aa4f42ed99
Yannik Braendle <yannik.braendle@t-online.de>
2025-09-05 00:14:26 +0200

    delete old test scene

--- pong/paddle_2.gd
+++
@@ -1,7 +0,0 @@
-class_name Paddle2
-extends CharacterBody2D
-
-func _physics_process(delta):
-	var test := Input.get_axis("ui_left", "ui_right")
-	velocity.y = test * 400.0
-	true if condigiont else false
