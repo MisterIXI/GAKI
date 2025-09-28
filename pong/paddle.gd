b2332ea8056916519a86ca66c3da1ca861eb4605
Yannik Braendle <yannik.braendle@t-online.de>
2025-09-29 00:46:41 +0200

    pong quick ball fix

--- pong/paddle.gd
+++ pong/paddle.gd
@@ -5,6 +5,10 @@ extends CharacterBody2D
 ## -1.0 down and 1.0 up
 var movement: float = 0.0
 
+func _ready():
+	var resource_id := get_rid()
+	PhysicsServer2D.body_set_continuous_collision_detection_mode(resource_id, PhysicsServer2D.CCD_MODE_CAST_RAY)
+
 func _physics_process(delta):
 	if movement != 0.0:
 		velocity.x = 0
