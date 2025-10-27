7edfdad5c827794b53e2fb9ad044da0f2b6f4a92
Yannik Braendle <yannik.braendle@t-online.de>
2025-10-28 00:29:41 +0100

    e02_07 lasers

---
+++ bot-brawl-exercise/scenes/weapons/laser_shot.gd
@@ -0,0 +1,48 @@
+class_name LaserShot
+extends Area2D
+
+var settings: ProjectileSetting
+@export var ray: RayCast2D
+var lifetime: float
+var base_velocity: Vector2
+var source_body: Node2D
+
+func init_laser(projectile_settings: ProjectileSetting, shooter: Node2D , initial_velocity: Vector2 = Vector2.ZERO):
+	settings = projectile_settings
+	base_velocity = initial_velocity
+	source_body = shooter
+
+func _ready():
+	add_to_group("laser")
+	if settings == null:
+		push_warning("No settings given on spawn. Self destructing...")
+		# queue_free()
+		settings = ProjectileSetting.new()
+		return
+	ray.target_position = Vector2.RIGHT * settings.laser_velocity
+	lifetime = settings.laser_lifetime
+
+func _physics_process(delta):
+	lifetime -= delta
+	if lifetime <= 0:
+		queue_free()
+		return
+	# move laser by laser_velocity/seconds
+	position += transform.x.normalized() * settings.laser_velocity * delta + base_velocity * delta
+
+func _on_body_entered(body: Node2D):
+	# to prevent multiple collisions after moving
+	if is_queued_for_deletion():
+		return
+	# exclude shooter
+	if body == source_body:
+		return
+	# check if body can be damaged
+	if body.is_in_group("has_health"):
+		# get health component of body and damage it
+		body.get_node(body.get_meta("health_path")).damage(settings.laser_damage)
+	if body.is_in_group("missile"):
+		var missile = body as Missile
+		missile.explode()
+	# self destruct
+	queue_free()
---
+++ bot-brawl-exercise/scenes/weapons/projectile_settings.gd
@@ -0,0 +1,30 @@
+class_name ProjectileSetting
+extends Resource
+
+@export var projectile_scene: PackedScene = null
+
+
+
+@export_group("Missile Settings")
+@export_custom(PROPERTY_HINT_GROUP_ENABLE,"") var is_missile: bool = false
+## fuel duration of missile until it runs out
+@export var fuel_duration: float = 5
+## Turning speed in degree/seconds
+@export var turning_speed: float = 30
+@export var turning_gain: float = 100
+@export var turning_damp: float = 10
+@export var velocity_alignment_rate: float = 1.0
+@export var boosting_strength: float = 50
+## Distance of detector polygon (FOV 45deg) in px
+@export var detector_distance: float = 180
+## Explosion radius
+@export var explosion_radius: float = 50
+@export var explosion_damage: float = 15
+@export var impact_damage: float = 10
+
+@export_group("Laser Settings")
+@export_custom(PROPERTY_HINT_GROUP_ENABLE, "") var is_laser: bool = false
+@export var laser_velocity: float = 50
+@export var laser_damage: float = 10
+@export var laser_energy_cost: float = 5
+@export var laser_lifetime: float = 3.0
